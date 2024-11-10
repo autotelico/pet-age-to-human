a2e0b76cc3c6a658462616ac7dd6a49bd25b34a7
Henrique Heron Magalhães <henriqueheronhh@gmail.com>
2024-11-10 19:22:24 -0300

    feat: store selected pet

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import './styles/App.css'
 import { Calculator } from './components/Calculator'
 
 import { petOptions } from './logic/options'
+import { Pet } from './logic/interfaces';
 
 function App() {
+  const [selectedPet, setSelectedPet] = useState<Pet | undefined>();
 
   return (
     <>
       <Calculator.Container>
         <>
-          <Calculator.PetSelector petOptions={petOptions}/>
-          <Calculator.SubmitButton cb={() => alert('cb!!')}/>
+        <h2>{selectedPet}</h2>
+          <Calculator.PetSelector pets={petOptions} setSelectedPet={setSelectedPet}/>
+          <Calculator.SubmitButton cb={() => alert(selectedPet)}/>
         </>
       </Calculator.Container>
     </>
--- src/components/Calculator/PetSelector/PetSelector.tsx
+++ src/components/Calculator/PetSelector/PetSelector.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import { ChangeEvent, Dispatch, SetStateAction } from "react";
 import { capitalize } from "../../../logic/customize-options";
+import { Pet } from "../../../logic/interfaces";
 
-export default function PetSelector({petOptions: pets}: {
-    petOptions: string[]
+export default function PetSelector({ pets, setSelectedPet }: {
+    pets: string[],
+    setSelectedPet: Dispatch<SetStateAction<Pet | undefined>>,
 }): JSX.Element {
+
+    const handleChange = (newPet: Pet) => {
+        setSelectedPet(newPet);
+        console.log(newPet);
+    }
+
     return (
-        <select name="species" id="species" defaultValue={""}>
+        <select name="species" id="species" defaultValue={""} onChange={(e: ChangeEvent<HTMLSelectElement>) => handleChange(e.target.value as Pet)}>
             <option value="">-</option>
             {pets.map((pet, i) =>
                 <option key={i} value={pet}>{capitalize(pet)}</option>
